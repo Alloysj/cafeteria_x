b79c8c06e3582d510069e21d13ad310ecac8b0b7
Alloysj <97891517+Alloysj@users.noreply.github.com>
2025-06-13 14:09:46 +0300

    feat(payments): notify orders service

--- apps/payments/src/payments.module.ts
+++ apps/payments/src/payments.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
 import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'ORDERS_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://localhost:5672'],
+          queue: 'orders_queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [PaymentsController],
   providers: [PaymentsService, PrismaService],
 })
